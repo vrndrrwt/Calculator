b3d45d57568dace3f1b288a667a13db8fc743c50
VIRENDRA SINGH RAWAT <115090559+vrndrrwt@users.noreply.github.com>
2023-05-23 16:42:47 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function appendToDisplay(value) {
+
+    var display = document.getElementById("display");
+
+    display.value += value;
+
+}
+
+ 
+
+function calculate() {
+
+    var display = document.getElementById("display");
+
+    var result = eval(display.value);
+
+    display.value = result;
+
+}
+
+ 
+
+function clearDisplay() {
+
+    var display = document.getElementById("display");
+
+    display.value = "";
+
+}
